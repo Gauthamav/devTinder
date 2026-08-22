b89f36a99af8fcf2863f2e2a531977503a24c790
gauthamav10 <goutham.zedsoft@gmail.com>
2026-08-22 17:22:06 +0530

    Route handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use('/hello', (req, res) => {
-  res.send('Hello from gduthadddm');
-});
+
+app.use('/hello',(req,res)=> {
+res.send("hello")
+})
+
 app.use('/', (req, res) => {
-  res.send('Helldo from the server');
+  res.send('Hello from the server');
 });
+
 
 app.listen(3000, () => {
   console.log('Server is successfully listening to port 3000....');
